ba3b1ca78a69532621be387a738b9cb99d151636
Dito <39705854+ditotet@users.noreply.github.com>
2022-07-27 16:02:02 +0400

    Return transactions after mining block

--- blockchain.py
+++ blockchain.py
@@ -107,6 +107,10 @@ class Blockchain:
 app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
 
+# Creating an adsress for the node on Port 5000
+node_address = str(uuid4()).replace('-', '')
+
+
 blockchain = Blockchain()
 
 @app.route('/mine_block', methods = ['GET'])
@@ -116,13 +120,18 @@ def mine_block():
 
     proof = blockchain.proof_of_work(previous_proof)
     previous_hash = blockchain.hash(previous_block)
+
+    blockchain.add_transaction(sender = node_address, receiver = 'Hadelin', amount = 1 )
+
     block = blockchain.create_block(proof, previous_hash)
+
     response = {
         'message': 'Congratulations, you just mined a block!',
         'index': block['index'],
         'timestamp': block['timestamp'],
         'proof': block['proof'],
-        'previous_hash': block['previous_hash']
+        'previous_hash': block['previous_hash'],
+        'transactions': block['transactions']
     }
 
     return jsonify(response), 200
